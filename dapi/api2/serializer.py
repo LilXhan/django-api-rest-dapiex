bf5b5c874d1ed5c89aa04e8aa032ce7c1531ff8e
Flavio <alvaradotuctod@gmail.com>
2023-02-05 09:21:06 -0500

    finish 1part

--- dapi/api2/serializer.py
+++ dapi/api2/serializer.py
@@ -4,4 +4,41 @@ from .models import Task
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task 
-        fields = ('name', 'priority', 'created_at', 'finished_at', )
+        fields = ('id', 'name', 'priority', 'created_at', 'finished_at', )
+
+    # def validate_name(self, value):
+        
+    #     condicion = True 
+    #     list = [str(i) for i in range(0, 10)]
+    #     while condicion:
+    #         for i in value:
+    #             if i in list:
+    #                 condicion = False
+    #         break;
+
+    #     if condicion == False:
+    #         raise serializers.ValidationError('Not numbers')
+        
+    #     return value
+
+    # def validate_priority(self, value):
+    #     if value is None: 
+    #         return value
+    #     if value < 1 or value > 3:
+    #         raise serializers.ValidationError('Range 1 and 3')
+    #     return value
+
+    def validate(self, data):
+        priority = data.get('priority')
+        name = data.get('name')
+
+        if contain_number(name):
+            raise serializers.ValidationError('Name not contain numbers')
+
+        if priority != None and (priority < 1 or priority > 3):
+            raise serializers.ValidationError('Range 1 and 3')
+
+        return data
+
+def contain_number(string):
+    return any(char.isdigit() for char in string)
--- dapi/api2/views.py
+++ dapi/api2/views.py
@@ -23,7 +23,7 @@ def tasks(request):
 
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT'])
 def task_completed(request):
